431c4d14ce983036c11a27ce57595a19a305c5bc
Roy Ratcliffe <roy@ratcliffe.me>
2026-07-08 17:45:56 +0100

    feat: add sysfs PWM read and write predicates
    Introduce sysfs_pwm_read/3 and sysfs_pwm_write/3 predicates to
    facilitate reading from and writing to PWM channel files in sysfs.
    These predicates ensure that the PWM channel is exported before
    performing read or write operations, streamlining the process for
    users.

--- prolog/sysfs/pwm.pl
+++ prolog/sysfs/pwm.pl
@@ -266,3 +266,254 @@ sysfs_pwm_chan(Chip, Export, Chan) :-
 % in the sysfs virtual file system using relative paths that are more convenient
 % and readable.
 user:file_search_path(sysfs_class_pwm, sysfs_class(pwm)).
+
+                /*******************************
+                *         THREE ARITY          *
+                *******************************/
+
+%!  sysfs_pwm_read(?Chip, ?Export, +Term) is nondet.
+%
+%   Reads a value from a file in  the   directory  of a PWM channel. The
+%   Term is a Prolog term that  specifies  the   file  to  read. It is a
+%   compound term with the name of the file  as the functor. The Chip is
+%   the name of the PWM class device,   such as pwmchip0, pwmchip1, etc.
+%   The Export is the number that   identifies the exported PWM channel.
+%   The Term's arguments are  unified  with   the  values  read from the
+%   corresponding files in the directory of   the PWM channel. The first
+%   argument unifies with the  value  read   from  the  file; the second
+%   argument, if present, specifies the unit of  the value (e.g., ns, s,
+%   hz, percent, fract). The predicate is   nondeterministic  and can be
+%   backtracked to read the values of multiple files in the directory of
+%   a PWM channel, or to read the values   of  the same file in multiple
+%   PWM channels.
+%
+%   Automatically ensures that the  specified   PWM  channel is exported
+%   before reading the file. If the channel  is not already exported, it
+%   will be exported by this predicate. This   allows the caller to read
+%   files from a PWM  channel  without   having  to  manually export the
+%   channel first, and ensures that the channel is exported when needed.
+%
+%   @arg Chip is the name of the PWM class device, such as pwmchip0,
+%   pwmchip1, etc.
+%
+%   @arg Export is the number that identifies the exported PWM channel.
+%
+%   @arg Term is a Prolog term that specifies the file to read, with the
+%   name of the file as the functor and  the value read from the file as
+%   the first argument. The second argument,  if any, specifies the unit
+%   of the value (e.g., ns, s, hz, percent, fract).
+
+sysfs_pwm_read(Chip, Export, Term) :- read_pwm(Term, Chip, Export).
+
+read_pwm(enable(Enable), Chip, Export) :-
+    sysfs_pwm_read(enable, Chip, Export, Enable).
+read_pwm(polarity(Polarity), Chip, Export) :-
+    sysfs_pwm_read(polarity, Chip, Export, Polarity).
+read_pwm(period(NS, ns), Chip, Export) :-
+    sysfs_pwm_read(period, Chip, Export, NS).
+read_pwm(period(S, s), Chip, Export) :-
+    read_pwm(period(NS, ns), Chip, Export),
+    s(NS, S).
+read_pwm(period(Hz, hz), Chip, Export) :-
+    read_pwm(period(NS, ns), Chip, Export),
+    hz(NS, Hz).
+read_pwm(duty_cycle(NS, ns), Chip, Export) :-
+    sysfs_pwm_read(duty_cycle, Chip, Export, NS).
+read_pwm(duty_cycle(S, s), Chip, Export) :-
+    read_pwm(duty_cycle(NS, ns), Chip, Export),
+    s(NS, S).
+read_pwm(duty_cycle(Hz, hz), Chip, Export) :-
+    read_pwm(duty_cycle(NS, ns), Chip, Export),
+    hz(NS, Hz).
+read_pwm(duty_cycle(Fract, fract), Chip, Export) :-
+    read_pwm(period(Period, ns), Chip, Export),
+    read_pwm(duty_cycle(DutyCycle, ns), Chip, Export),
+    Fract is DutyCycle / Period.
+read_pwm(duty_cycle(Percent, percent), Chip, Export) :-
+    read_pwm(duty_cycle(Fract, fract), Chip, Export),
+    Percent is 100 * Fract.
+
+%!  sysfs_pwm_write(+Chip, +Export, ++Term) is det.
+%
+%   Writes a value to a file in the directory of a PWM channel. The Term
+%   is a Prolog term that specifies the file  to write. It is a compound
+%   term with the name of the file as  the functor. The Chip is the name
+%   of the PWM class device, such as pwmchip0, pwmchip1, etc. The Export
+%   is the number that identifies the   exported PWM channel. The Term's
+%   arguments are the values to write to  the corresponding files in the
+%   directory of the PWM channel. The  first   argument  is the value to
+%   write to the file; the second   argument,  if present, specifies the
+%   unit of the value (e.g., ns, s,   hz, percent, fract). The predicate
+%   is deterministic and will succeed  if   the  values are successfully
+%   written to the files, and will fail if  there is an error writing to
+%   any of the files, such as if a   file does not exist, if the channel
+%   is not exported and  cannot  be  exported,   if  a  value  cannot be
+%   converted to a string, or  if  there   is  a  permission  error when
+%   writing to a file. The  predicate   automatically  ensures  that the
+%   specified PWM channel is exported before   writing  to the files. If
+%   the channel is not already exported,  it   will  be exported by this
+%   predicate. This allows the caller to write to files in a PWM channel
+%   without having to manually export  the   channel  first, and ensures
+%   that the channel is exported when needed.
+%
+%   @arg Chip is the name of the PWM class device, such as pwmchip0,
+%   pwmchip1, etc.
+%
+%   @arg Export is the number that identifies the exported PWM channel.
+%
+%   @arg Term is a Prolog term that   specifies  the file to write, with
+%   the name of the file as the functor   and  the value to write to the
+%   file as the first argument. The   second argument, if any, specifies
+%   the unit of the value (e.g., ns, s, hz, percent, fract).
+
+sysfs_pwm_write(Chip, Export, Term) :- write_pwm(Term, Chip, Export).
+
+write_pwm(enable(Enable), Chip, Export) :-
+    sysfs_pwm_write(enable, Chip, Export, Enable).
+write_pwm(polarity(Polarity), Chip, Export) :-
+    sysfs_pwm_write(polarity, Chip, Export, Polarity).
+write_pwm(period(NS, ns), Chip, Export) :-
+    NS_ is round(NS),
+    sysfs_pwm_write(period, Chip, Export, NS_).
+write_pwm(period(S, s), Chip, Export) :-
+    s(NS, S),
+    write_pwm(period(NS, ns), Chip, Export).
+write_pwm(period(Hz, hz), Chip, Export) :-
+    hz(NS, Hz),
+    write_pwm(period(NS, ns), Chip, Export).
+write_pwm(duty_cycle(NS, ns), Chip, Export) :-
+    NS_ is round(NS),
+    sysfs_pwm_write(duty_cycle, Chip, Export, NS_).
+write_pwm(duty_cycle(S, s), Chip, Export) :-
+    s(NS, S),
+    write_pwm(duty_cycle(NS, ns), Chip, Export).
+write_pwm(duty_cycle(Hz, hz), Chip, Export) :-
+    hz(NS, Hz),
+    write_pwm(duty_cycle(NS, ns), Chip, Export).
+write_pwm(duty_cycle(Fract, fract), Chip, Export) :-
+    read_pwm(period(Period, ns), Chip, Export),
+    DutyCycle is Fract * Period,
+    write_pwm(duty_cycle(DutyCycle, ns), Chip, Export).
+write_pwm(duty_cycle(Percent, percent), Chip, Export) :-
+    Fract is Percent / 100,
+    write_pwm(duty_cycle(Fract, fract), Chip, Export).
+
+%!  s(NS, S) is det.
+%
+%   Converts between nanoseconds and  seconds.  NS   is  the  number  of
+%   nanoseconds, and S is the number of seconds. If NS is a variable, it
+%   will be unified with the number   of  nanoseconds corresponding to S
+%   seconds. If S is a variable, it will   be unified with the number of
+%   seconds  corresponding  to  NS   nanoseconds.    The   predicate  is
+%   deterministic and will succeed if the  conversion is successful, and
+%   fail otherwise.
+%
+%   @arg NS is the number of nanoseconds, which is a float.
+%   @arg S is the number of seconds, which is a float.
+
+s(NS, S), var(NS) => NS is 1_000_000_000 * S.
+s(NS, S) => S is NS / 1_000_000_000.
+
+%!  hz(NS, Hz) is det.
+%
+%   Converts  between  nanoseconds  and  Hertz.  NS  is  the  number  of
+%   nanoseconds, and Hz is the frequency in  Hertz. If NS is a variable,
+%   it will be unified with the   number of nanoseconds corresponding to
+%   Hz Hertz. If Hz is a variable, it will be unified with the frequency
+%   in  Hertz  corresponding  to  NS    nanoseconds.  The  predicate  is
+%   deterministic and will succeed if the  conversion is successful, and
+%   fail otherwise.
+%
+%   @arg NS is the number of nanoseconds, which is a float.
+%   @arg Hz is the frequency in Hertz, which is a float.
+
+hz(NS, Hz), var(NS) => NS is 1_000_000_000 / Hz.
+hz(NS, Hz) => NS > 0, Hz is 1_000_000_000 / NS.
+
+                /*******************************
+                *          TWO ARITY           *
+                *******************************/
+
+%!  sysfs_pwm(?Chip, ?Export, ?PWM) is nondet.
+%!  sysfs_pwm(?PWM) is nondet.
+%
+%   Within sysfs, a PWM channel  is   represented  as  a directory named
+%   pwmN, where N is the number of   the channel. The directory contains
+%   files  that  provide  information   about    the   channel  and  its
+%   capabilities, such as enable, period,  duty_cycle, and polarity. The
+%   pwmN directory appears within the  chip   directory,  which is named
+%   pwmchipN, where N is the number of the   chip.  Hence, the path to a
+%   PWM   channel   in    sysfs    is     typically    of    the    form
+%   /sys/class/pwm/pwmchipN/pwmM, where N is the number  of the chip and
+%   M is the number of  the  channel.   Consequently,  a  PWM channel is
+%   uniquely identified by the  combination  of   its  chip  and channel
+%   numbers.
+%
+%   @arg Chip is the name of a PWM class device, which is a directory in
+%   the /sys/class/pwm directory that contains a file named `npwm`.
+%
+%   @arg Export is the number that identifies the exported PWM channel.
+%
+%   @arg PWM is a compound term of the form Chip(Chan), where Chan is the
+%   name of the file in the sysfs virtual file system that corresponds to
+%   the exported PWM channel, which is of the form pwmN, where N is the
+%   number of the PWM channel. The term uniquely identifies a PWM channel
+%   in sysfs by combining the chip and channel numbers.
+
+sysfs_pwm(Chip, Export, PWM) :-
+    sysfs_pwm_chan(Chip, Export, Chan),
+    PWM =.. [Chip, Chan].
+
+sysfs_pwm(PWM) :- sysfs_pwm(_, _, PWM).
+
+%!  sysfs_pwm_read(?PWM, +Term) is nondet.
+%
+%   Reads a value from a file in  the   directory  of a PWM channel. The
+%   Term is a Prolog term that  specifies  the   file  to  read. It is a
+%   compound term with the name of the file as the functor. The PWM is a
+%   compound term of the form Chip(Chan), where  Chan is the name of the
+%   file in the sysfs  virtual  file   system  that  corresponds  to the
+%   exported PWM channel, which is of  the   form  pwmN,  where N is the
+%   number of the PWM channel. The Term's arguments are unified with the
+%   values read from the corresponding files in the directory of the PWM
+%   channel. The first argument unifies  with   the  value read from the
+%   file; the second argument, if  present,   specifies  the unit of the
+%   value  (e.g.,  ns,  s,  hz,  percent,    fract).  The  predicate  is
+%   nondeterministic and can be  backtracked  to   read  the  values  of
+%   multiple files in the directory of  a   PWM  channel, or to read the
+%   values of the same file in multiple PWM channels.
+%
+%   @arg PWM is a compound term of   the  form Chip(Chan), where Chan is
+%   the name of  the  file  in  the   sysfs  virtual  file  system  that
+%   corresponds to the exported PWM channel, which  is of the form pwmN,
+%   where N is the number of the PWM channel.
+%
+%   @arg Term is a Prolog term that specifies the file to read, with the
+%   name of the file as the functor and  the value read from the file as
+%   the first argument. The second argument,  if any, specifies the unit
+%   of the value (e.g., ns, s, hz, percent, fract).
+
+sysfs_pwm_read(PWM, Term) :-
+    sysfs_pwm(Chip, Export, PWM),
+    sysfs_pwm_read(Chip, Export, Term).
+
+%!  sysfs_pwm_write(?PWM, ++Term) is nondet.
+%
+%   Writes a value to a file in the directory of a PWM channel.
+%
+%   The Term is a Prolog term that specifies  the file to write. It is a
+%   compound term with the name of the file as the functor; the compound arguments specify what to write.
+%
+%   @arg PWM is a compound term of   the  form Chip(Chan), where Chan is
+%   the name of  the  file  in  the   sysfs  virtual  file  system  that
+%   corresponds to the exported PWM channel, which  is of the form pwmN,
+%   where N is the number of the PWM channel.
+%
+%   @arg Term is a Prolog term that   specifies  the file to write, with
+%   the name of the file as the functor   and  the value to write to the
+%   file as the first argument. The   second argument, if any, specifies
+%   the unit of the value (e.g., ns, s, hz, percent, fract).
+
+sysfs_pwm_write(PWM, Term) :-
+    sysfs_pwm(Chip, Export, PWM),
+    sysfs_pwm_write(Chip, Export, Term).
