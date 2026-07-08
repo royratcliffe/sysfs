9d255b046e08f34877e37ab2b2f319e4ed695d14
Roy Ratcliffe <roy@ratcliffe.me>
2026-07-08 17:48:30 +0100

    feat: add overloaded sysfs_gpio_read and sysfs_gpio_write predicates
    Introduce additional overloads for sysfs_gpio_read and
    sysfs_gpio_write predicates to enhance functionality and
    simplify usage. This includes new arity variations for
    handling GPIO line operations more effectively.

--- prolog/sysfs/gpio.pl
+++ prolog/sysfs/gpio.pl
@@ -32,11 +32,14 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             sysfs_gpio_exported/4, % ?Chip, ?Offset, -Export, -Line
             sysfs_gpio_ensure_exported/4, % ?Chip, ?Offset, -Export, -Line
             sysfs_gpio_ensure_unexported/4, % ?Chip, ?Offset, -Export, -Line
-            sysfs_gpio_read/3, % ?File, ?Line, -Data
             sysfs_gpio_read/4, % ?File, ?Chip, ?Offset, -Data
-            sysfs_gpio_write/3, % ?File, ?Line, +Data
+            sysfs_gpio_read/3, % ?File, ?Line, -Data
+            sysfs_gpio_read/2, % ?Line, +Term
             sysfs_gpio_write/4, % ?File, ?Chip, ?Offset, +Data
-            sysfs_gpio_line/4 % ?Chip, ?Offset, -Export, -Line
+            sysfs_gpio_write/3, % ?File, ?Line, +Data
+            sysfs_gpio_write/2, % ?Line, ++Term
+            sysfs_gpio_line/4 % ?Chip, ?Offset, ?Export, ?Line
+          , sysfs_gpio_line/3 % ?Chip, ?Offset, ?Line
           ]).
 :- use_module(library(sysfs)).
 :- use_module(library(sysfs/gpiochip)).
